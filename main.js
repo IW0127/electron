e3198875a40fda3f1f5df004ff1b97f662f317d4
Hardik Dholairya <hardik.dholariya@identixweb.in>
2025-05-09 16:18:08 +0530

    set for mac

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const { Worker } = require('worker_threads');
 const { app, BrowserWindow, screen, ipcMain, net, Menu } = require('electron');
 const common = require('./resources/function');
-
+const path = require('path');
 let worker;
 
 let mainWindow;
+
+const filePath = app.isPackaged ? process.resourcesPath : __dirname;
+
+const workerpath = path.join(filePath, 'resources', 'timer-worker.js');
 
 app.on('ready', () => {
     /* worker thread start */
@@ -12,14 +16,18 @@ app.on('ready', () => {
         try {
             let data = await net.fetch('https://testhrms-api.identixweb.com/node/admin_api/getLocalTime');
             data = await data.json();
-            worker = new Worker(__dirname + '/resources/timer-worker.js', { workerData: { serverNow: new Date(data.data).getTime() } });
+            worker = new Worker(workerpath, { workerData: { serverNow: new Date(data.data).getTime() } });
+            worker.on('message', ({ serverTime }) => {
+                if (mainWindow && !mainWindow.isDestroyed()) {
+                    mainWindow.webContents.send('tick', new Date(serverTime));
+                }
+            });
         } catch (error) {
             console.error('Error fetching local time:', error);
             // Handle the error as needed, e.g., show a message to the user or retry the request
         }
     })();
     /* worker thread end */
-
     createWindow();
     // const menu = Menu.buildFromTemplate([]);
     // Menu.setApplicationMenu(menu);
@@ -27,7 +35,7 @@ app.on('ready', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
 });
-console.log('app ready');
+
 app.on('window-all-closed', () => {
     console.log('window-all-closed');
     worker.terminate();
@@ -39,13 +47,10 @@ app.on('window-all-closed', () => {
 async function createWindow() {
     let data = await net.fetch('https://testhrms-api.identixweb.com/node/admin_api/getLocalTime');
     data = await data.json();
-    const worker = new Worker(__dirname + '/resources/timer-worker.js', { workerData: { serverNow: new Date(data.data).getTime() } });
-
+    worker = new Worker(workerpath, { workerData: { serverNow: new Date(data.data).getTime() } });
     const displays = screen.getAllDisplays();
-
     const secondDisplay = displays[1] || displays[0];
     const { width, height } = screen.getPrimaryDisplay().bounds;
-
     mainWindow = new BrowserWindow({
         x: secondDisplay.bounds.x + 50,
         y: secondDisplay.bounds.y + 50,
@@ -54,24 +59,12 @@ async function createWindow() {
         minWidth: width,
         minHeight: height,
         webPreferences: {
-            preload: __dirname + '/resources/preload.js',
+            preload: path.join(__dirname, 'resources', 'preload.js'),
             // devTools: true,
             webviewTag: true
         },
     });
-
-    // mainWindow.loadURL('https://testhrms.identixweb.com');
-    mainWindow.loadURL('http://localhost:3001');
+    mainWindow.loadURL('https://testhrms.identixweb.com');
+    // mainWindow.loadURL('http://localhost:3001');
     mainWindow.webContents.openDevTools();
-    mainWindow.webContents.send('tick', new Date(data.data));
-    worker.on('message', ({ serverTime }) => mainWindow.webContents.send('tick', new Date(serverTime)));
-    /* console.log(app);
-    app.on('window-all-closed', function () {
-        if (process.platform !== 'darwin') app.quit();
-    }); */
-    // ipcMain.emit('systemInfo', getSystemInfo());
-
-    // mainWindow.webContents.on('did-finish-load', () => {
-    //     mainWindow.webContents.send('systemInfo', getSystemInfo());
-    // });
 }
